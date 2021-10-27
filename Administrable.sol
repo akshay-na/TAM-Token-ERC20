1049ad4222401013fa55881acd64b58b9b0f0d11
akshay-na <akshayna04698@gmail.com>
2021-10-27 19:31:09 +0530

    Added custom admin module

---
+++ Administrable.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.17;
+
+contract Administrable {
+    address private _admin;
+
+    event AdminshipTransferred(
+        address indexed currentAdmin,
+        address indexed newAdmin
+    );
+
+    constructor() {
+        _admin = msg.sender;
+        emit AdminshipTransferred(address(0), _admin);
+    }
+
+    function admin() public view returns (address) {
+        return _admin;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == _admin, "Only admin can perform this action");
+        _;
+    }
+
+    function tranferAdminship(address newAdmin) public onlyAdmin {
+        emit AdminshipTransferred(msg.sender, newAdmin);
+        _admin = newAdmin;
+    }
+}
